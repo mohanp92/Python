3463f956d6ad6bc344f5db19bb4c3e2a06bb2160
Mohan Pawar <pawar.mohan102@gmail.com>
2019-08-09 22:51:15 +0530

    Missing Value added

--- Important Commands.py
+++ Important Commands.py
@@ -6,5 +6,34 @@ def missingvalue(df):
     missing_df=pd.concat([total,percent], axis=1, keys=['Total','Precent'])
     return missing_df
     -----------------------------
+    
+Replace missing value
+titanic.Survived.replace(to_replace= ["yes", "no"], value = [1, 0], inplace = True)
 
+Rename the columns
+summer.rename(columns = {"Athlete Name": "Athlete_Name"}, inplace = True)
+---------------------------------------------------------------------------
+summer.Medal.replace(to_replace= "Gold Medal", value = "Gold", inplace = True)
+-----------------------------------------------------------------------------
+To Replace string from the column
+titanic.Fare = titanic.Fare.str.replace("$", "")
+------------------------------------------------------------------------------
+To Search something from the column  then use str.contain(")
+summer.loc[summer.Athlete.str.contains("HAJOS")]
+---------------------------------------------------------------
+Convert the vlaues in to numeric then use pd.numeric
+ pd.to_numeric(titanic.Fare)
+--------------------------------------------------
+To Change the data type of column then
+pd.to_numeric(titanic.Fare) then
+use astype titanic["Fare"] = titanic.Fare.astype("float")
+ --------------------------------------------------------
+ we will get the only missing value rows and columns
+  titanic.loc[titanic.isna().any(axis=1)]
+ ------------------------------------------------------------
+                                                          
+                                                          
+                                                          
+                                                          
+                                                          
     
